c97695e8e19cf159015d71adafb9f53c660d3b0b
RtC <me@mydomain.com>
2021-12-16 13:07:04 +0100

    admin site

--- src/main/java/com/randomshop/shop/service/ImageStorageService.java
+++ src/main/java/com/randomshop/shop/service/ImageStorageService.java
@@ -11,6 +11,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
 @Service
@@ -42,7 +44,7 @@ public class ImageStorageService{
     }
 
     public String save(MultipartFile file) {
-        String imgName = (getRandomString() + LocalDateTime.now() + "_" + file.getOriginalFilename()).replace(":", "_");
+        String imgName = (getRandomString(6) + LocalDateTime.now() + "_" + file.getOriginalFilename()).replace(":", "_");
         try {
             Files.copy(file.getInputStream(), Paths.get(this.root).resolve(imgName),
                     StandardCopyOption.REPLACE_EXISTING);
@@ -53,7 +55,7 @@ public class ImageStorageService{
     }
 
     public String saveSiteImg(MultipartFile file) {
-        String imgName = (getRandomString() + LocalDateTime.now() + "_" + file.getOriginalFilename()).replace(":", "_");
+        String imgName = (getRandomString(6) + LocalDateTime.now() + "_" + file.getOriginalFilename()).replace(":", "_");
         try {
             Files.copy(file.getInputStream(), Paths.get(this.site).resolve(imgName),
                     StandardCopyOption.REPLACE_EXISTING);
@@ -66,8 +68,9 @@ public class ImageStorageService{
     public String saveSiteLogo(MultipartFile file) {
         String ori = file.getOriginalFilename();
         assert ori != null;
-        String imgName = "logo".concat(ori.substring(ori.lastIndexOf(".")));
+        String imgName = getRandomString(3) + LocalTime.now().format(DateTimeFormatter.ofPattern("HHmmss")) + "logo".concat(ori.substring(ori.lastIndexOf(".")));
         try {
+            deleteLogo();
             Files.copy(file.getInputStream(), Paths.get(this.site).resolve(imgName),
                     StandardCopyOption.REPLACE_EXISTING);
         } catch (Exception e) {
@@ -84,10 +87,19 @@ public class ImageStorageService{
         }
     }
 
-    private String getRandomString() {
+    public void deleteLogo() throws IOException {
+        Files.list(Paths.get(this.site)).filter(p -> p.toString().contains("logo")).forEach((p) ->{
+            try {
+                Files.deleteIfExists(p);
+            }catch (Exception e) {
+                System.out.println(e);
+            }
+        });
+    }
+
+    private String getRandomString(int targetStringLength) {
         int leftLimit = 48; // numeral '0'
         int rightLimit = 122; // letter 'z'
-        int targetStringLength = 10;
 
         return random.ints(leftLimit, rightLimit + 1)
                 .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
